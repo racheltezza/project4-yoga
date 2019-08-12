40b322db3e22db886053206d5e03f2b9853c2926
Rachel Tezza <racheletezza@gmail.com>
2019-08-12 10:49:54 -0400

    initial commit

---
+++ client/build/precache-manifest.fcd134c1c44addcca7ba1c88a580dd86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970259b010ae8ad4d0b6f46cc7a7176c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0f664fd828a24617218",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "d0f664fd828a24617218",
+    "url": "/static/js/main.fd31daed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
