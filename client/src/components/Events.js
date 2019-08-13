874e148cbcf058bb17ddd2544708c01e6fc2fdf6
Rachel Tezza <racheletezza@gmail.com>
2019-08-13 15:43:28 -0400

    add event form working

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -36,6 +36,10 @@ export default class Events extends Component {
     *   -REMINDER remember `setState` it is an async function
     */
     componentDidMount() {
+        this.getAllEvents()
+    }
+    
+    getAllEvents = () => {
         axios.get(`/api/users/${this.props.match.params.userId}/eventsLists/${this.props.match.params.listId}/events`)
             .then((res) => {
                 this.setState({events: res.data})
@@ -46,6 +50,27 @@ export default class Events extends Component {
         axios.delete(`/api/users/${this.props.match.params.userId}/eventsLists/${this.props.match.params.listId}`)
         .then(() => {
             this.setState({redirectToEventsList: true})
+        })
+    }
+
+    handleInputChange = (event) => {
+        let copiedNewEvent = {...this.state.newEvent}
+        copiedNewEvent[event.target.name] = event.target.value
+        this.setState({newEvent: copiedNewEvent})
+    }
+
+    handleNewEventSubmit = (event) => {
+        event.preventDefault()
+        axios.post(`/api/users/${this.props.match.params.userId}/eventsLists/${this.props.match.params.listId}/events`, this.state.newEvent)
+        .then(() => {
+            this.setState({isNewEventFormShowing: false})
+            this.getAllEvents()
+        })
+    }
+
+    handleToggleNewEventForm = () => {
+        this.setState((state) => {
+          return {isNewEventFormShowing: true}
         })
     }
 
@@ -63,11 +88,28 @@ export default class Events extends Component {
             return <Link to={`/users/${this.props.match.params.userId}/eventsLists/${this.props.match.params.listId}/events/${event._id}`}>{event.name}</Link>
         })
         return (
+            this.state.isNewEventFormShowing
+            ?
+            <form onSubmit={this.handleNewEventSubmit}>
+                <label htmlFor='new-event-name'>Event Name: </label>
+                <input type='text' name='name' id='new-event-name' onChange={this.handleInputChange}/>
+                <label htmlFor='new-event-date'>Event Date: </label>
+                <input type='text' name='date' id='new-event-date' onChange={this.handleInputChange}/>
+                <label htmlFor='new-event-city'>Event City: </label>
+                <input type='text' name='city' id='new-event-city' onChange={this.handleInputChange}/>
+                <label htmlFor='new-event-address'>Event Address: </label>
+                <input type='text' name='address' id='new-event-address' onChange={this.handleInputChange}/>
+                <label htmlFor='new-event-description'>Event Description: </label>
+                <input type='text' name='description' id='new-event-description' onChange={this.handleInputChange}/>
+                <input type='submit' value='Add Event' />
+            </form>
+            :
             <div>
                 {/* Accessing the value of message from the state object */}
                 <Link to={`/users/${this.props.match.params.userId}/eventsLists/`}>Back to all Lists</Link>
                 <h1>One List</h1>
                 {eventsList}
+                <button onClick={this.handleToggleNewEventForm}>Add New Event</button>
                 <button onClick={this.handleDeleteEventsList}>Delete this List</button>
             </div>
         )
