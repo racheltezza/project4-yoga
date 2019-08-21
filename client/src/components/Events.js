509fd8682af32a08f2b167a84ad9dc344211679d
Rachel Tezza <racheletezza@gmail.com>
2019-08-21 10:05:02 -0400

    clean

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,6 +1,4 @@
-/* Step 1 import React, { Component } and axios
- *
- */
+
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Link, Redirect } from 'react-router-dom'
@@ -18,10 +16,6 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import YogaImage from '../yoga.jpg'
 
-/* Step 2
- * Rename this class to reflect the component being created
- *
- */
 let AUTHORIZATION = 'Bearer' + ' ' + process.env.REACT_APP_EVENTBRITE_API_KEY
 
 
@@ -115,11 +109,8 @@ const MyButton = styled(Button)({
         const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.eventbriteapi.com/v3/events/search?location.address=${city}&location.within=10km&expand=venue&categories=107&subcategories=7005`, {method: 'GET', headers: {'Authorization': AUTHORIZATION}});
         const data = await api_call.json()
         if(city) {
-            console.log(data)
             this.setState({
                 eventsSearched: data.events})
-            console.log(this.state.eventName)
-            console.log(this.state.eventsSearched)
         } else {
             this.setState({error: 'Please enter a city'})
         }
@@ -133,59 +124,43 @@ const MyButton = styled(Button)({
         let searchedEventsList = this.state.eventsSearched.map((event) => {
             return (
                 <div>
-                {/* <img src={event.logo.url} alt="Yoga-Event"/>
-                <p>Event: {event.name.text} </p>
-                <p>Address: {event.venue.address.address_1}</p>
-                <li>Date/Time: {event.start.local}</li>
-                <p>{event.description.text}</p> */}
-     <Card className='card'>
-      <CardHeader
-        title={event.name.text}
-        subheader={event.start.local} 
-      />
-      <CardMedia
-        className='media'
-        image={event.logo===null ? YogaImage : event.logo.url}
-        title={event.name.text.substring(0, 20)}
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-        <p>{event.venue.address.address_1}</p>
-        <p>{event.description.text ? event.description.text.substring(0, 100) + "..." : ''}</p>
-        </Typography>
-      </CardContent>
-    </Card>
+                    <Card className='card'>
+                    <CardHeader
+                        title={event.name.text}
+                        subheader={event.start.local} 
+                    />
+                    <CardMedia
+                        className='media'
+                        image={event.logo===null ? YogaImage : event.logo.url}
+                        title={event.name.text.substring(0, 20)}
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                        <p>{event.venue.address.address_1}</p>
+                        <p>{event.description.text ? event.description.text.substring(0, 100) + "..." : ''}</p>
+                        </Typography>
+                    </CardContent>
+                    </Card>
                 </div>
             )
         })
         let eventsList = this.state.events.map((event) => {
             return (
                 <MyListItem className='event-list-item'>
-                <ListItemIcon>
-                  <EventIcon />
-                </ListItemIcon>
-                <Link to={`/users/${this.props.match.params.userId}/eventsLists/${this.props.match.params.listId}/events/${event._id}`}>{event.name}</Link>
-              </MyListItem>
+                    <ListItemIcon>
+                        <EventIcon />
+                    </ListItemIcon>
+                    <Link to={`/users/${this.props.match.params.userId}/eventsLists/${this.props.match.params.listId}/events/${event._id}`}>{event.name}</Link>
+                </MyListItem>
             )
         })
         return (
             this.state.isNewEventFormShowing
             ?
             <form onSubmit={this.handleNewEventSubmit}>
-                {/* <label htmlFor='new-event-name'>Event Name: </label>
-                <input type='text' name='name' id='new-event-name' onChange={this.handleInputChange}/>
-                <label htmlFor='new-event-date'>Event Date: </label>
-                <input type='text' name='date' id='new-event-date' onChange={this.handleInputChange}/>
-                <label htmlFor='new-event-city'>Event City: </label>
-                <input type='text' name='city' id='new-event-city' onChange={this.handleInputChange}/>
-                <label htmlFor='new-event-address'>Event Address: </label>
-                <input type='text' name='address' id='new-event-address' onChange={this.handleInputChange}/>
-                <label htmlFor='new-event-description'>Event Description: </label>
-                <input type='text' name='description' id='new-event-description' onChange={this.handleInputChange}/> */}
                 <TextField
                 id="standard-uncontrolled"
                 label="Event Name"
-                // defaultValue="foo"
                 name='name'
                 margin="normal"
                 onChange={this.handleInputChange}
@@ -225,30 +200,26 @@ const MyButton = styled(Button)({
                 <Link to={`/users/${this.props.match.params.userId}/eventsLists/`} className="nav-link-back">Back to all Lists</Link>
                 <h1>One List{this.state.name}</h1>
                 <div className='list-action-button'>
-                <MyButton onClick={this.handleToggleNewEventForm}>Add New Event</MyButton>
+                    <MyButton onClick={this.handleToggleNewEventForm}>Add New Event</MyButton>
                 </div>
                 <List className='lists-list'>
-                {eventsList}
+                    {eventsList}
                 </List>
                 <DeleteButton onClick={this.handleDeleteEventsList}>Delete this List</DeleteButton>
-                {/* <Button onClick={this.handleToggleNewEventForm} variant="outlined" color="primary">Add New Event</Button> */}
                 <div className='event-search'>
                     <h4 className='seach-label'>Browse Yoga events by city:</h4>
-                <form onSubmit={this.getEventbriteEvents}>
-                    {/* <label htmlFor='city-search'>Browse events by city:</label> */}
-                    {/* <input id='city-search' type='text' placeholder='city...' name='city' /> */}
-                    <TextField
-                        id="outlined-name"
-                        label="City"
-                        name='city'
-                        margin="normal"
-                        variant="outlined"
-                        className='search-input'
-                    />
+                    <form onSubmit={this.getEventbriteEvents}>
+                        <TextField
+                            id="outlined-name"
+                            label="City"
+                            name='city'
+                            margin="normal"
+                            variant="outlined"
+                            className='search-input'
+                        />
                     <input type='submit' value="Search" className='search-input'/>
-                </form>
+                    </form>
                 </div>
-                {/* <SearchEvents/> */}
                 <div className='search-items'>
                     {searchedEventsList}
                 </div>
